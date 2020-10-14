3070fba5b34eb904962ea813750f516d99d9d926
rsi2m <sergei@plumbr.eu>
2020-10-14 09:04:10 +0300

    minor fixes

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -184,35 +184,4 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         super.onRestoreInstanceState(savedInstanceState)
         Timber.i("onRestoreInstanceState Called")
     }
-
-    /** Lifecycle Methods **/
-    override fun onStart() {
-        super.onStart()
-        Timber.i("onStart Called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Timber.i("onResume Called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Timber.i("onPause Called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Timber.i("onStop Called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Timber.i("onDestroy Called")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Timber.i("onRestart Called")
-    }
 }
